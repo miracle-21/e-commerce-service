f0947f92bfe19ec77f0c04a5bb913a2692dee113
minha <miraculum95@gmail.com>
2022-09-14 18:42:49 +0900

    Feat: 결제 내역 조회 기능 생성
    Feat: 결제 내역 조회 기능 생성
    
    Feat: 결제 내역 조회 기능 생성

--- payments/views.py
+++ payments/views.py
@@ -2,9 +2,10 @@ import json
 
 from django.http      import JsonResponse
 from django.views     import View
+from django.db.models import Q
 
 from core.token      import token_user
-from products.models import Product
+from products.models import Product, ProductDetail
 from users.models    import User
 from payments.models import Payment
 
@@ -12,7 +13,11 @@ class PaymentView(View):
     @token_user
     def post(self, request, product_id):
         '''
-        기능: 상품 결제 기능. 유저 정보는 따로 명시하지 않는 이상 가입된 기본 정보를 가지고 온다.
+        상품 결제 기능
+
+        Notice:
+            - 결제 API는 구현하지 못해서 회원 credit에서 금액이 차감되는 형태로 구현.
+            - credit도 실제 돈이 들어가야 하니 따로 충전 기능은 구현하지 않은 상태.
         '''
         try:
             product = Product.objects.get(id=product_id)
@@ -40,7 +45,7 @@ class PaymentView(View):
                     orderer = orderer,
                     phone = phone,
                     address = address,
-                    status = '1'
+                    status = "결제완료"
                 )
 
                 item.count -= count
@@ -52,3 +57,48 @@ class PaymentView(View):
         
         except Product.DoesNotExist:
             return JsonResponse({'message' : '존재하지 않는 상품입니다'}, status = 400)
+
+    @token_user
+    def get(self, request):
+        '''
+        결제 내역 조회
+        관리자: 전체 회원 내역 조회 가능, 검색 기능 사용 가능.
+        일반회원: 본인 결제 내역만 조회 가능. 검색 기능 사용 가능.
+        '''
+        address = request.GET.get('address')
+
+
+        q = Q()
+
+        if address:
+            q &= Q(address__contains = address)
+        
+        FILTER_SET = {
+            'product' : 'product',
+            'phone' : 'phone',
+            'status' : 'status',
+            }
+
+        filter = { FILTER_SET.get(key) : value for key, value in request.GET.items() if FILTER_SET.get(key) }
+        
+        if request.user.role == False:
+            payments = Payment.objects.filter(q).filter(**filter)
+        else:
+            payments = Payment.objects.filter(q).filter(**filter).filter(user_id=request.user.id)
+
+        
+        results = [
+            {
+                'user_id' : payment.user_id,
+                'product_id' : payment.product_id,
+                'product_name' : ProductDetail.objects.get(product_id=payment.product_id).name,
+                'price' : int(payment.price),
+                'user_name' : User.objects.get(id=payment.user_id).name,
+                'orderer' : payment.orderer,
+                'phone' : payment.phone,
+                'address' : payment.address,
+                'status' : payment.status,
+            } for payment in payments
+        ]
+        return JsonResponse({'results' : results}, status = 200)
+
